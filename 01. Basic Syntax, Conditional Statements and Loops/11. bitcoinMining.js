d1567b3f62bb5232bae8a892bad16c541ab0fed5
NadezhdaGeorgieva <nadezhdageorgieva@outlook.com>
2021-05-24 22:13:49 +0300

    improved variables naming

--- 01. Basic Syntax, Conditional Statements and Loops/11. bitcoinMining.js
+++ 01. Basic Syntax, Conditional Statements and Loops/11. bitcoinMining.js
@@ -1,21 +1,22 @@
-function mining(input) {
+function bitcoinMining(input) {
 
-  let gains = 0.0, gramOfGold = 67.51, BTC = 11949.16;
-  let firstBTC = 0, hasBTC = false;
+  let gains = 0.0, gramOfGold = 67.51, btc = 11949.16;
+  let dayOfFirstBTC = 0, hasBTC = false;
   for(let i = 0; i < input.length; i++) {
     let dailyGains = gramOfGold * input[i];
     if((i + 1) % 3 === 0) { // every 3 days
       dailyGains -= (30/100) * dailyGains; // 30 pct less money
     }
     gains += dailyGains;
-    if(gains >= BTC && !hasBTC) { hasBTC = true; firstBTC = i + 1; } // get first day you purchased bitcoin
+    if(gains >= btc && !hasBTC) { hasBTC = true; dayOfFirstBTC = i + 1; } // get first day you purchased bitcoin
   }
   
-  let ownedBTC = Math.floor(gains / BTC);
-  
+  let ownedBTC = Math.floor(gains / btc);
   console.log(`Bought bitcoins: ${ownedBTC}`)
+
   if(hasBTC) {
-  	console.log(`Day of the first purchased bitcoin: ${firstBTC}` + '\n' + `Left money: ${(gains - (BTC * ownedBTC)).toFixed(2)} lv.`)
+    let moneyLeft = gains - (btc * ownedBTC);
+  	console.log(`Day of the first purchased bitcoin: ${dayOfFirstBTC}` + '\n' + `Left money: ${moneyLeft.toFixed(2)} lv.`)
   } else {
     console.log(`Left money: ${gains.toFixed(2)} lv.`)
   }
